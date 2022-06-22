d2b8b05da9f2e910b7c7a6c97c7b25c2653457b7
ThePursuitofCode <thepursuitofcode@gmail.com>
2022-06-21 21:31:17 -0600

    Updates by Pursuit of Code

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-// Rock, Paper, Scissors
 
-// Create a function that acts as the computer that will randomly return rock, paper, or scissors.
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
-const computerSelection = computerPlay()
 
-function computerPlay(){
-    let randomNum = Math.floor(Math.random() * 3) + 1
-    if (randomNum === 1){
-        return "rock"
-    } else if (randomNum === 2){
-        return "paper"
-    } else if (randomNum === 3){
-        return "scissors"
+function playRound(){
+    const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
+    const computerSelection = computerPlay()
+
+    function computerPlay(){
+        let randomNum = Math.floor(Math.random() * 3) + 1
+        if (randomNum === 1){
+            return "rock"
+        } else if (randomNum === 2){
+            return "paper"
+        } else if (randomNum === 3){
+            return "scissors"
+        }
     }
-}
-
-// Create a function that plays a single round taking in the player's selection and computer's selection.
-// Declare if player wins or loses.
-// Make the player selection not case sensitive.
-
-function playRound(playerSelection, computerSelection){
+    console.log(`The player picked ${playerSelection}`)
     console.log(`The computer picked ${computerSelection}`)
+
     if(playerSelection === computerSelection){
         return "It's a tie!"
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
@@ -38,4 +34,7 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-console.log(playRound(playerSelection, computerSelection))
+
+for(let i = 0; i < 5; i++){
+    console.log(playRound())
+}
